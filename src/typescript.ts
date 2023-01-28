eaf25666c832c5ea1927d2dc2a85dd34cc01c06d
OPT0463 <49981930+OPT0463@users.noreply.github.com>
2023-01-28 10:47:07 +0900

    ㅂ

---
+++ src/typescript.ts
@@ -0,0 +1,15 @@
+const username: string = 'KIM KIHYUN';
+const email: string = 'kkh555999@gmail.com';
+const numberPassword: string = '000000';
+
+const accountList: string[] = [
+    username,
+    email,
+    numberPassword
+];
+
+console.table(accountList);
+
+console.log(username);
+console.log(email);
+console.log(numberPassword);
